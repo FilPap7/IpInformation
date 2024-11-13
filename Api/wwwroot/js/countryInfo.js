111641bc66960896ea9cb459632cc9205b704218
fpappas <fpappas@01solutions.com>
2024-11-13 16:15:27 +0200

    Added Pagination to the ListIP Page

--- Api/wwwroot/js/countryInfo.js
+++ Api/wwwroot/js/countryInfo.js
@@ -30,28 +30,86 @@ function getCountryInfo() {
     });
 }
 
-// Function to get all country information
+let currentPage = 1;
+const rowsPerPage = 10;
+
 function getAllCountryInfo() {
     $.ajax({
         url: '/Ip/GetAllCountryWithIpInfo',
         method: 'GET',
         success: function (ipData) {
-            const $tableBody = $('#ip-table-body');
-            $tableBody.empty(); // Clear any existing rows
-
-            ipData.forEach(function (ip) {
-                const row = `<tr>
-                                <td>${ip.ip}</td>
-                                <td>${ip.countryName}</td>
-                                <td>${ip.twoLetterCode}</td>
-                                <td>${ip.threeLetterCode}</td>
-                             </tr>`;
-                $tableBody.append(row);
-            });
+            renderTable(ipData, currentPage);
+            createPaginationControls(ipData);
         },
         error: function () {
-            console.error('There was an error retrieving stored IP information.');
-            alert('Error retrieving stored IP information. Please try again.');
+            console.error('Error retrieving IP information.');
+            alert('Please try again.');
         }
     });
+}
+
+function renderTable(data, page) {
+    const $tableBody = $('#ip-table-body');
+    $tableBody.empty();
+
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    const paginatedData = data.slice(start, end);
+
+    paginatedData.forEach(function (ip) {
+        const row = `<tr>
+                        <td>${ip.ip}</td>
+                        <td>${ip.countryName}</td>
+                        <td>${ip.twoLetterCode}</td>
+                        <td>${ip.threeLetterCode}</td>
+                     </tr>`;
+        $tableBody.append(row);
+    });
+}
+function createPaginationControls(data) {
+    const totalPages = Math.ceil(data.length / rowsPerPage);
+    const $paginationControls = $('#pagination-controls');
+    $paginationControls.empty();
+
+    // Pagination Wrapper
+    const paginationList = $('<ul class="pagination justify-content-center"></ul>');
+
+    // Previous Button
+    const prevItem = $('<li class="page-item"><button class="page-link">Previous</button></li>');
+    if (currentPage === 1) prevItem.addClass('disabled');
+    prevItem.on('click', function () {
+        if (currentPage > 1) {
+            currentPage--;
+            renderTable(data, currentPage);
+            createPaginationControls(data);
+        }
+    });
+    paginationList.append(prevItem);
+
+    // Page Number Buttons
+    for (let i = 1; i <= totalPages; i++) {
+        const pageItem = $(`<li class="page-item"><button class="page-link">${i}</button></li>`);
+        if (i === currentPage) pageItem.addClass('active');
+        pageItem.on('click', function () {
+            currentPage = i;
+            renderTable(data, currentPage);
+            createPaginationControls(data);
+        });
+        paginationList.append(pageItem);
+    }
+
+    // Next Button
+    const nextItem = $('<li class="page-item"><button class="page-link">Next</button></li>');
+    if (currentPage === totalPages) nextItem.addClass('disabled');
+    nextItem.on('click', function () {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderTable(data, currentPage);
+            createPaginationControls(data);
+        }
+    });
+    paginationList.append(nextItem);
+
+    // Append to controls div
+    $paginationControls.append(paginationList);
 }
