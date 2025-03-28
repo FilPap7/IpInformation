294f268ba517d5a101eea63b53b957c0d0873efe
fpappas <fpappas@01solutions.com>
2025-03-28 11:57:31 +0200

    Added a Password Manager

---
+++ Common/PasswordManager/IPasswordManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Common.PasswordManager
+{
+    internal interface IPasswordManager
+    {
+        string HashPassword(string password);
+        bool ValidatePassword(string password, string hashedPassword);
+    }
+}
